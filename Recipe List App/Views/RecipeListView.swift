41cb696cd2ea37fbbf35484b9e58ded542d15a41
Matt <matt.spong@gmail.com>
2021-02-15 16:18:18 +1100

    - rename file 'ContentView.swift' to 'RecipeListView.swift' and rename all references to this class - change swiftui list to scrollview, lazyvstack and foreach - hide navigationbartitle and use text element for the title in RecipeListView

---
+++ Recipe List App/Views/RecipeListView.swift
@@ -0,0 +1,62 @@
+//
+//  RecipeListView.swift
+//  Recipe List App
+//
+//  Created by Matthew Spong on 5/2/21.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    
+    // Reference the view model
+    @EnvironmentObject var model:RecipeModel
+    
+    var body: some View {
+        
+        
+        
+        NavigationView {
+            
+            VStack (alignment: .leading) {
+                
+                Text("All Recipes")
+                    .bold()
+                    .padding(.top, 40)
+                    .font(.largeTitle)
+                
+                ScrollView {
+                
+                    LazyVStack (alignment: .leading) {
+                        ForEach(model.recipes) { r in
+                        
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe:r),
+                                label: {
+                                    HStack(spacing: 20.0) {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                    }
+                                })
+                        }
+                    }
+                }
+            }
+            .navigationBarHidden(true)
+            .padding(.leading)
+        }
+    }
+}
+
+struct RecipeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeListView()
+            .environmentObject(RecipeModel())
+    }
+}
--- Recipe List App/Views/RecipeTabView.swift
+++ Recipe List App/Views/RecipeTabView.swift
@@ -17,7 +17,7 @@ struct RecipeTabView: View {
                     Text("Featured")
                 }
             
-            ContentView()
+            RecipeListView()
                 .tabItem {
                     Image(systemName: "list.bullet")
                     Text("List")
